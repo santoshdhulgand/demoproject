1a39b8b26e56227f4de9b7a72226dabbe0c1217c
santosh <santoshdhulgand81195@gmail.com>
2021-06-10 21:59:04 +0530

    updated urls.py

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -7,7 +7,7 @@ from demoapp import views
 urlpatterns = [
     path('base/' , views.BasePageView.as_view()),
     #path('read/' , views.get_student_data)
-    path('read/' , views.StudentListView.as_view()),
+    path('' , views.StudentListView.as_view()),
     path('create/' , views.StudentCreateView.as_view() , name = 'student_create'),
     path('detail/<int:id>/' , views.StudentDetailView.as_view() , name = 'student_detail'),
     path('update/<int:id>/' , views.StudentUpdateView.as_view() , name = 'student_update'),
